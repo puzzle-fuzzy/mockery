78996df55f4c82901600acd42d2c520a6687802a
18267094443@163.com <16615812538@163.com>
2026-07-07 15:36:40 +0800

    change

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,9 +10,4 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss()
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })
